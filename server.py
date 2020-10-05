968348124a063ff8af5f116b22844365faa69edb
Sachin Saini <iamsainisachin@gmail.com>
2020-10-05 21:15:23 +0530

    initial commit

---
+++ server.py
@@ -0,0 +1,44 @@
+import pickle
+import keras
+import requests
+from bs4 import BeautifulSoup
+from keras.preprocessing.sequence import pad_sequences
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+tokenizer = None
+with open("newsfn-word-index.pickle", "rb") as handle:
+    tokenizer = pickle.load(handle)
+
+model = keras.models.load_model("newsfn.h5")
+word_index = tokenizer.word_index
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    content = request.json
+    url = content["url"]
+    text = fetch_text(url)
+    inp = [word_index[word] for word in text.split() if word in word_index]
+    p = pad_sequences([inp], maxlen=500, padding="post", truncating="post")
+    score = model.predict(p)[0][0]
+    data = {"score": str(score)}
+    headers = {"Content-Type": "application/json"}
+    return jsonify(data), headers
+
+
+def fetch_text(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    c = soup.find("div", id=lambda x: x and x.startswith("content-body-"))
+    ch = c.findChildren("p", recursive=False, text=True)
+    text = ""
+    for child in ch:
+        text += child.getText()
+    return text
+
+
+if __name__ == "__main__":
+    app.run(threaded=True, port=5000)
+
